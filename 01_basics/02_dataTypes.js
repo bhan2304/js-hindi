a51d032b10c43eeafac01be5946c0f353f4333b2
bhan2304 <82810516+bhan2304@users.noreply.github.com>
2023-12-23 17:27:38 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+console.log(3 + 3)
+
+console.log("Boby")
+
+let name = " hitesh"
+let age = 18
+let isLoggedIn = false
+
+//  number => 2 to the power 53 
+// bigInt 
+// string = " "
+// boolean => true / false 
+// null => standalone value 
+// undefined =>
+// symbol => unique
+// object 
+
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
